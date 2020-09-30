017b00854e4be37bc9373770ffa31eb588053a86
Joe Bigio <jvbigio@gmail.com>
2020-09-30 08:44:17 -0600

    added canvas and styling

---
+++ app.js
@@ -0,0 +1,9 @@
+let canvas
+let canvasContext
+
+window.onload = function () {
+  canvas = document.getElementById('gameCanvas')
+  canvasContext = canvas.getContext('2d')
+  canvasContext.fillStyle = 'green'
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+}
