95c0169392885acffbe8a53689e6a6b4933af351
Joe Bigio <jvbigio@gmail.com>
2020-09-30 08:49:30 -0600

    added snake and apple

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ window.onload = function () {
   canvasContext = canvas.getContext('2d')
   canvasContext.fillStyle = 'green'
   canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  canvasContext.fillStyle = 'blue'
+  // below 100px from left, 200px from top, 50px wide, 25px tall:
+  canvasContext.fillRect(100, 200, 75, 25)
+  canvasContext.fillStyle = 'red'
+  canvasContext.fillRect(230, 230, 25, 25)
 }
