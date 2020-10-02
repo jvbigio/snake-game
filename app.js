2643eb5d3205c230c3244050fab9f14dfd0c35e7
Joe Bigio <jvbigio@gmail.com>
2020-10-02 08:19:11 -0600

    control listeners set

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let canvas
 let canvasContext
-let snakeX = 50
+const snakeX = 50
 const snakeY = 50
 const appleX = 50
 
@@ -13,6 +13,33 @@ window.onload = function () {
     moveSnake()
     drawApple()
   }, 50)
+  // works:
+  // window.addEventListener('keydown', e => {
+  //   if (e.key === 'ArrowUp') {
+  //     console.log('Up arrow press!')
+  //   }
+}
+
+window.addEventListener('keydown', gameControls)
+
+function gameControls (e) {
+  // if (e.key === 'ArrowUp') {
+  //   console.log('Up arrow press!')
+  // }
+  switch (e.key) {
+    case 'ArrowUp':
+      console.log('Up arrow press!')
+      break
+    case 'ArrowRight':
+      console.log('Right arrow press!')
+      break
+    case 'ArrowDown':
+      console.log('Down arrow press!')
+      break
+    case 'ArrowLeft':
+      console.log('Left arrow press!')
+      break
+  }
 }
 
 function drawCanvas () {
@@ -21,8 +48,8 @@ function drawCanvas () {
 }
 
 function moveSnake () {
-  snakeX += 10
-
+  // snakeX += 10
+  // snakeY += 10
   canvasContext.fillStyle = 'blue'
   // below 100px from left, 200px from top, 50px wide, 25px tall:
   canvasContext.fillRect(snakeX, 200, 50, 20)
