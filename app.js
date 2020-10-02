99cd5f0c0dd061d01d7bed0f9171f97bc3b4e96a
Joe Bigio <jvbigio@gmail.com>
2020-10-02 07:45:36 -0600

    snake moves on canvas

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 let canvas
 let canvasContext
+let snakeX = 50
+const snakeY = 50
+const appleX = 50
 
 window.onload = function () {
   canvas = document.getElementById('gameCanvas')
   canvasContext = canvas.getContext('2d')
+
+  setInterval(() => {
+    drawCanvas()
+    moveSnake()
+    drawApple()
+  }, 50)
+}
+
+function drawCanvas () {
   canvasContext.fillStyle = 'green'
   canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+function moveSnake () {
+  snakeX += 10
+
   canvasContext.fillStyle = 'blue'
   // below 100px from left, 200px from top, 50px wide, 25px tall:
-  canvasContext.fillRect(100, 200, 75, 25)
+  canvasContext.fillRect(snakeX, 200, 50, 20)
+}
+
+function drawApple () {
+  // appleX
   canvasContext.fillStyle = 'red'
-  canvasContext.fillRect(230, 230, 25, 25)
+  canvasContext.fillRect(appleX, 100, 20, 20)
 }
