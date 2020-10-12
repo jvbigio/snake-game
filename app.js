c7993331e46f854b66e77e2ffc6b90fedd400952
Joe Bigio <jvbigio@gmail.com>
2020-10-12 19:23:50 -0400

    Working on movement bug

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 let canvas
 let canvasContext
 const snakeHeight = 20
-const snakeSpeedX = 20
-const snakeSpeedY = 20
+const moveX = 20
 const appleX = 200
 const appleY = 400
-const deltaY = 20
+const moveY = 20
 
 const snakeBody = [
-  { x: 60, y: 80 }, // head
+  { x: 60, y: 80 },
   { x: 40, y: 80 },
   { x: 20, y: 80 },
-  { x: 0, y: 80 } // tail
+  { x: 0, y: 80 }
 ]
 
 window.onload = function () {
@@ -32,39 +31,63 @@ window.addEventListener('keydown', gameControls)
 function gameControls (e) {
   switch (e.key) {
     case 'ArrowUp':
-      snakeBody[0].y <= 0 ? alert('Snake hit top wall. Game over!') : snakeBody[0].y -= deltaY
+      snakeBody[0].y <= 0 ? alert('Snake hit top wall. Game over!') : snakeBody[0].y -= moveY
       for (let i = 1; i < snakeBody.length; i++) {
         const snakePart = snakeBody[i]
 
-        snakePart.x < snakeBody[0].x ? snakePart.x += snakeSpeedX : snakePart.y -= deltaY
+        snakePart.x < snakeBody[0].x ? snakePart.x += moveX : snakePart.y -= moveY
       }
       break
     case 'ArrowRight':
-      snakeBody[0].x === canvas.width - 20 ? alert('Snake hit right wall. Game over!') : snakeBody[0].x += snakeSpeedX
-
+      snakeBody[0].x === canvas.width - 20 ? alert('Snake hit right wall. Game over!') : snakeBody[0].x += moveX
       for (let i = 1; i < snakeBody.length; i++) {
         const snakePart = snakeBody[i]
 
-        snakePart.y < snakeBody[0].y ? snakePart.y += deltaY : snakePart.x += snakeSpeedX
+        // starting position
+        if (snakePart.y === snakeBody[0].y) {
+          snakePart.x += moveX
+        } else if (snakePart.y < snakeBody[0].y) { // head vertical down
+          snakePart.y += moveY
+        } else if (snakePart.y > snakeBody[0].y) { // head vertical up
+          snakePart.y -= moveY
+        }
       }
       break
     case 'ArrowDown':
-      snakeBody[0].y >= canvas.height - snakeHeight ? alert('Snake hit bottom wall. Game over!') : snakeBody[0].y += deltaY
+      snakeBody[0].y >= canvas.height - snakeHeight ? alert('Snake hit bottom wall. Game over!') : snakeBody[0].y += moveY
 
       for (let i = 1; i < snakeBody.length; i++) {
         const snakePart = snakeBody[i]
 
-        snakePart.x < snakeBody[0].x ? snakePart.x += snakeSpeedX : snakePart.y += deltaY
+        // starting position
+        if (snakePart.x < snakeBody[0].x) {
+          snakePart.x += moveX
+        } else if (snakePart.y < snakeBody[0].y) { // head vertical down
+          snakePart.y += moveY
+        } else if (snakePart.y > snakeBody[0].y) { // head vertical up
+          snakePart.y -= moveY
+        // head facing left
+        } else if (snakePart.x > snakeBody[0].x) {
+          snakePart.x -= moveX
+        }
       }
       break
     case 'ArrowLeft':
-      snakeBody[0].x <= 0 ? alert('Snake hit left wall. Game Over!') : snakeBody[0].x -= snakeSpeedX
+      snakeBody[0].x <= 0 ? alert('Snake hit left wall. Game Over!') : snakeBody[0].x -= moveX
 
       for (let i = 1; i < snakeBody.length; i++) {
         const snakePart = snakeBody[i]
 
-        snakePart.y < snakeBody[0].y ? snakePart.y += deltaY : snakePart.x -= snakeSpeedX
+        // head facing left
+        if (snakePart.y === snakeBody[0].y && snakePart.x > snakeBody[0].x) {
+          snakePart.x -= moveX
+        } else if (snakePart.y < snakeBody[0].y) { // head vertical down
+          snakePart.y += moveY
+        } else if (snakePart.y > snakeBody[0].y) { // head vertical up
+          snakePart.y -= moveY
+        }
       }
+
       break
   }
 }
@@ -94,5 +117,3 @@ function eatApple () {
     console.log('collision with apple!')
   }
 }
-
-
